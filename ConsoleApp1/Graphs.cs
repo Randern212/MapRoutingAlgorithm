fae26f5416560b96d2c99960cb38e51b95e4ca6e
randern212 <randern212@gmail.com>
2025-05-16 19:04:18 +0300

    added vertex and edge subclasses

--- ConsoleApp1/Graphs.cs
+++ ConsoleApp1/Graphs.cs
@@ -6,9 +6,22 @@ using System.Threading.Tasks;
 
 namespace Graphs
 {
+    class Vertex
+    {
+        public float positionX;
+        public float positionY;
+    }
+
+    class Edge
+    {
+        public float length;
+        public float speed;
+        public float time;
+    }
+
     class Graph
     { 
-    
+
     }
 
     class GraphConstructor
