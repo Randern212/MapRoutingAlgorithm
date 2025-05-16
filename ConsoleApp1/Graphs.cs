b2ef8f7cca3ee3290a42d63b04b1bb4b9eff09e4
randern212 <randern212@gmail.com>
2025-05-17 01:46:04 +0300

    wrote the findpath function recursively

--- ConsoleApp1/Graphs.cs
+++ ConsoleApp1/Graphs.cs
@@ -11,23 +11,44 @@ namespace Graphs
     {
         public float positionX;
         public float positionY;
-        Dictionary<int, Edge> edges;
+        public Dictionary<int, Edge> edges;
     }
-
     class Edge
     {
         public float length;
         public float speed;
         public float time => length/speed;
     }
-
     class Graph
     { 
         Dictionary<int, Vertex> vertices;
-        public void findPath(int rootID)
+        public void findPath(int rootID,float rootTime, Queue<int> visitedVertices)
         {
+            visitedVertices.Enqueue(rootID);
 
+            float[] timeFromRoot = new float[vertices.Count];
+            for (int i = 0; i < vertices.Count; i++)
+            {
+                timeFromRoot[i] = float.MaxValue;
+            }
+            timeFromRoot[rootID] = rootTime;
+            
+            int nextRootID = -1;
+            float nextRootTime=float.MaxValue;
+            
+            foreach(var edge in vertices[rootID].edges)
+            {
+                timeFromRoot[edge.Key] = edge.Value.time + rootTime;
+                if (nextRootTime > timeFromRoot[edge.Key])
+                {
+                    nextRootID = edge.Key;
+                    nextRootTime = timeFromRoot[edge.Key];
+                }
+            }
+
+            findPath(nextRootID, nextRootTime, visitedVertices);
         }
+
     }
 
     class GraphConstructor
