ebef2f59c9c65f069cdfe2d32ed95f84fadf2df1
randern212 <randern212@gmail.com>
2025-05-17 19:01:00 +0300

    wrote the euclideanDistance lambda expression to be used in measureTrip function

--- ConsoleApp1/Queries.cs
+++ ConsoleApp1/Queries.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,6 +16,7 @@ namespace Queries
         float destinationY;
         float R;
         public Queury() { }
+        Func<float, float, float, float,float> euclideanDistance = (x1, y1, x2, y2) => MathF.Sqrt(((x1-x2)*(x1-x2))+((y1-y2)*(y1-y2))) ; 
         public void measureTrip()
         {
 
